1d8227a39227c76e53f6aecd0a478c17f4152ebc
Lautaro-Lencina <rautaro.x2020@gmail.com>
2026-02-13 11:56:36 -0300

    Actalizar

--- combinarPDF.py
+++ combinarPDF.py
@@ -1,17 +1,24 @@
 import os
+from tkinter import Tk, filedialog, messagebox
 from PyPDF4 import PdfFileMerger
-#lee la carpeta donde esta el programa
-carpeta_entrada =  os.getcwd()
 
+Tk().withdraw()#oculta ventana principal
+messagebox.showinfo("","coloque la carpeta con los pdfs.(nota: el orden es segun como esten en la carpeta)")
+carpeta_entrada = filedialog.askdirectory()
+#lee la carpeta donde esta el programa 
+    
 def fucionpdf(carpeta_entrada, archivo_salida = 'fucion.pdf'):
-
-    pdfs = []
-    mango = PdfFileMerger()
-    for archivo in os.listdir(carpeta_entrada):
-        if archivo[-4:] == ".pdf":
-            pdfs.append(archivo)
-    for pdf in pdfs:
-        mango.append(str(pdf))
-    mango.write(archivo_salida)
-    mango.close()
+    if carpeta_entrada:
+     pdfs = []
+     mango = PdfFileMerger()
+    
+     for archivo in os.listdir(carpeta_entrada):
+      if archivo[-4:] == ".pdf":
+        pdfs.append(archivo)
+        ruta = carpeta_entrada + '\\' + archivo
+        with open(ruta, 'rb') as fileobj:
+                mango.append(fileobj)
+     archivo_salida = os.path.join(carpeta_entrada, archivo_salida)#solucion de error de la ruta
+     mango.write(archivo_salida)
+     mango.close()
 fucionpdf(carpeta_entrada)
